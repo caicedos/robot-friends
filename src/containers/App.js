defcd67338d93c61832f2b6497a496135ddc8f46
Flash <42217856+caicedos@users.noreply.github.com>
2019-10-29 22:25:22 -0400

    added redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,55 +1,51 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox'
-import Scroll from '../components/Scroll'
-import './App.css'
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import { connect } from 'react-redux';
+import './App.css';
+
+import { setSearchField, requestRobots } from '../actions';
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
+    }
+}
 
 class App extends Component {
 
-    constructor(){
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-        console.log(this.state, "constructor")
+    componentDidMount() {
+        this.props.onRequestRobots()
     }
 
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(user => {
-                this.setState({robots:user})
-                console.log(this.state, 'Componentdimount')
-            })
-            console.log('actual componentdidmount')
-    }
-    
-    
-    
-    onSearchChange = (event) => {
-        this.setState({
-            searchfield: event.target.value
+    render() {
+        const { searchField, onSearchChange, robots, isPending } = this.props
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
         })
-        console.log(this.state,'custome change state')
+        return isPending ?
+            <h1>Loading</h1> :
+            (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            )
     }
-    
-    
-    render(){
-        console.log(this.state, 'render')
-        const filteredRobots = this.state.robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-        })
-        return(
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Scroll>
-                  <CardList robots={filteredRobots}/>  
-                </Scroll>
-            </div>
-        )  
-    }  
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./containers/App";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
+import { searchRobots, requestRobots } from './reducers'
 import "tachyons";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const logger = createLogger()
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
 
 serviceWorker.unregister();
